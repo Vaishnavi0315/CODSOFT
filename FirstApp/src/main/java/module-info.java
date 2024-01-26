12d20d6fdb7296e1165ccb4e61ad5ca76c6899a0
Vaishnavi0315 <72209987+Vaishnavi0315@users.noreply.github.com>
2024-01-26 20:08:22 +0530

    Delete FirstApp directory

--- FirstApp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.firstapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.com.vsb.javafx to javafx.fxml;
-    exports com.example.com.vsb.javafx;
-}
